117b7a0452a605bfaa9b1b204a432f322bacd3a5
Pedro Kretzschmar <pedro.kretz@gmail.com>
2021-08-02 18:13:28 -0300

    Stylized top contributors table

--- src/components/TopContributors.js
+++ src/components/TopContributors.js
@@ -1,36 +1,72 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Box, GU, IdentityBadge, textStyle } from '@commonsswarm/ui'
+import { GU } from '@commonsswarm/ui'
 
 const TopContributors = ({ contributors = [] }) => {
   return (
-    <Box heading="Top Contributors">
-      {contributors.map(({ account, formattedTotalValue }) => (
+    <Wrapper>
+      <TitleContent>
+        <WrapperTitle>Top 10 contributors</WrapperTitle>
+        <WrapperSubTitle>Receiving the NFT</WrapperSubTitle>
+      </TitleContent>
+      {contributors.map(({ account, formattedTotalValue }, index) => (
         <Contributor key={account}>
-          <IdentityBadge entity={account} />
-          <div
-            title={formattedTotalValue}
-            css={`
-              ${textStyle('body3')}
-              margin-left: ${3 * GU}px;
-              overflow: hidden;
-              white-space: nowrap;
-              text-overflow: ellipsis;
-            `}
-          >
-            {formattedTotalValue}
-          </div>
+          <Position>{index + 1}. </Position>
+          <Position>{`${account.slice(0, 9)}...${account.slice(-9)}`}</Position>
+          <Contribuition>{`${formattedTotalValue.slice(
+            0,
+            -6
+          )} wxDAI`}</Contribuition>
         </Contributor>
       ))}
-    </Box>
+    </Wrapper>
   )
 }
 
+const Wrapper = styled.div`
+  background-color: inherit;
+  max-width: 380px;
+  @media (max-width: 900px) {
+    margin: auto;
+  }
+`
+
+const TitleContent = styled.div`
+  margin-bottom: 32px;
+`
+
+const WrapperTitle = styled.div`
+  font-family: 'Inter';
+  font-size: 18px;
+  font-weight: 700;
+`
+
+const WrapperSubTitle = styled.div`
+  font-family: 'Inter';
+  font-size: 14px;
+  font-weight: 400;
+`
+
 const Contributor = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: right;
   align-items: center;
   margin-bottom: ${2 * GU}px;
+`
+
+const Position = styled.div`
+  font-family: 'Archivo';
+  font-size: 18px;
+  font-weight: 400;
+  padding-right: 8px;
+  width: 32.7px;
+`
+
+const Contribuition = styled.div`
+  font-family: 'Archivo';
+  font-size: 18px;
+  font-weight: 400;
+  margin-left: auto;
 `
 
 export default TopContributors
