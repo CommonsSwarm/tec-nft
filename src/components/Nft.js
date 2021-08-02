37ef5747923d7cc39f9e71e5d87646dd922e5d9c
Pedro Kretzschmar <pedro.kretz@gmail.com>
2021-08-02 18:14:11 -0300

    Stylized bot img, timer and min bid card

--- src/components/Nft.js
+++ src/components/Nft.js
@@ -1,30 +1,75 @@
-import React from 'react'
-import { Button, Timer, Split, GU, textStyle } from '@commonsswarm/ui'
+import React, { useEffect, useState } from 'react'
+import { Button, Split, GU } from '@commonsswarm/ui'
 import styled from 'styled-components'
 
 function Nft({ minBid, endDate }) {
+  const generateTimeDisplay = () => {
+    const rightJustNow = new Date().getTime()
+    const runway = endDate - rightJustNow
+    const stateObj = {
+      days: Math.floor(runway / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((runway % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      minutes: Math.floor((runway % (1000 * 60 * 60)) / (1000 * 60)),
+      seconds: Math.floor((runway % (1000 * 60)) / 1000),
+    }
+    return stateObj
+  }
+
+  const [timeDisplay, setTimeDisplay] = useState(generateTimeDisplay)
+  useEffect(() => {
+    setTimeDisplay(generateTimeDisplay)
+    const interval = setInterval(
+      () => setTimeDisplay(generateTimeDisplay),
+      1000
+    )
+
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <div>
       <Split
-        primary={<img src="/bot.png" alt="hatch bot" />}
+        primary={<BotImg src="/bot.png" alt="hatch bot" />}
         secondary={
           <>
             <Title>The hatch bot</Title>
-            <h2>
+            <SubTitle>
               The top 10 participants will each receive this limited edition
               NFT.
-            </h2>
-            <div>
-              <div>Minimum to participate</div>
-              {minBid}
-            </div>
-            <div>
-              <div>Hatch ending in</div>
-              <Timer end={endDate} />
-            </div>
-            <Button href="https://hatch.tecommons.org">
+            </SubTitle>
+            <Wrapper>
+              <div>
+                <Requirements>Minimum to participate</Requirements>
+                <Currency>
+                  {minBid ? minBid.slice(0, -5) : ''}
+                  <CurrencyType>wxDAI</CurrencyType>
+                </Currency>
+              </div>
+              <div>
+                <Requirements>Hatch ending in</Requirements>
+                <CounterWrapper>
+                  <DayCounter>
+                    <CounterContent>{timeDisplay.days}</CounterContent>
+                    <CounterSubtitle>Days</CounterSubtitle>
+                  </DayCounter>
+                  <TimeCounter>
+                    <CounterContent>{timeDisplay.hours}</CounterContent>
+                    <CounterSubtitle>Hours</CounterSubtitle>
+                  </TimeCounter>
+                  <TimeCounter>
+                    <CounterContent>{timeDisplay.minutes}</CounterContent>
+                    <CounterSubtitle>Minutes</CounterSubtitle>
+                  </TimeCounter>
+                  <TimeCounter>
+                    <CounterContent>{timeDisplay.seconds}</CounterContent>
+                    <CounterSubtitle>Seconds</CounterSubtitle>
+                  </TimeCounter>
+                </CounterWrapper>
+              </div>
+            </Wrapper>
+            <CustomButton href="https://hatch.tecommons.org">
               Participate in the hatch
-            </Button>
+            </CustomButton>
           </>
         }
         secondaryWidth={`${75 * GU}px`}
@@ -33,10 +78,117 @@ function Nft({ minBid, endDate }) {
   )
 }
 
+const BotImg = styled.img`
+  box-shadow: -20px -20px #defb48;
+  @media (max-width: 900px) {
+    display: flex;
+    margin: auto;
+  }
+`
+
 const Title = styled.h1`
-  ${textStyle('title1')};
-  text-transform: uppercase;
+  font-family: 'Bai Jamjuree Bold';
   font-size: 66px;
+  font-weight: 700;
+  text-transform: uppercase;
+  @media (max-width: 900px) {
+    text-align: center;
+  }
+`
+
+const SubTitle = styled.h2`
+  font-family: 'Inter';
+  font-size: 16px;
+  font-weight: 400;
+  @media (max-width: 900px) {
+    text-align: center;
+  }
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  max-width: 500px;
+  margin-top: 15%;
+  margin-bottom: 40px;
+  @media (max-width: 900px) {
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    margin: 5% auto;
+  }
+`
+
+const Requirements = styled.div`
+  font-family: 'Bai Jamjuree';
+  font-size: 18px;
+  font-weight: 400;
+`
+
+const CurrencyType = styled.div`
+  font-family: 'Inter';
+  font-size: 18px;
+  font-weight: 400;
+  display: inline;
+`
+
+const Currency = styled.div`
+  font-family: 'Inter';
+  font-size: 44px;
+  font-weight: 700;
+`
+
+const CounterWrapper = styled.div`
+  display: flex;
+  @media (max-width: 900px) {
+    justify-content: center;
+  }
+`
+
+const DayCounter = styled.div`
+  font-family: 'Inter';
+  font-size: 44px;
+  font-weight: 700;
+  color: white;
+  padding: 0px 10px;
+  display: flex;
+  flex-direction: column;
+  align-self: flex-end;
+`
+
+const TimeCounter = styled.div`
+  font-family: 'Inter';
+  font-size: 24px;
+  font-weight: 500;
+  color: white;
+  padding: 0px 10px;
+  display: flex;
+  flex-direction: column;
+  align-self: flex-end;
+`
+
+const CounterContent = styled.div`
+  align-self: flex-start;
+`
+
+const CounterSubtitle = styled.div`
+  font-family: 'Inter';
+  font-size: 13px;
+  font-weight: 400;
+  color: white;
+`
+
+const CustomButton = styled(Button)`
+  font-family: 'Bai Jamjuree Bold';
+  font-size: 18px;
+  font-weight: 700;
+  text-transform: uppercase;
+  width: 370px;
+  height: 60px;
+  @media (max-width: 900px) {
+    display: flex;
+    margin: auto;
+  }
 `
 
 export default Nft
